f3a41b1eb27c39a69c4d83701c7ca4444c755a73
“yangchaojianggit config --global user.email www.1007181167@qq.com <www.1007181167@qq.com>
2017-09-22 13:12:58 +0800

    修改模板

---
+++ Tengxunyun/src/main/java/com/tengxunyun/utils/MyDownLoadListener.java
@@ -0,0 +1,11 @@
+package com.tengxunyun.utils;
+
+/**
+ * Created by yangc on 2017/8/9.
+ * E-Mail:yangchaojiang@outlook.com
+ * Deprecated:
+ */
+
+public class MyDownLoadListener {
+    public static final String TAG = "MyDownLoadListener";
+}
---
+++ Tengxunyun/src/main/java/com/tengxunyun/utils/MyLoadTaskListener.java
@@ -0,0 +1,12 @@
+package com.tengxunyun.utils;
+
+/**
+ * Created by yangc on 2017/8/9.
+ * E-Mail:yangchaojiang@outlook.com
+ * Deprecated: 上专回调
+ */
+
+public interface MyLoadTaskListener {
+
+    void  onSuccess();
+}
